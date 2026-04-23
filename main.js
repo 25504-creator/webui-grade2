c8a26d880d5822d14132f7152e784f8b499c19a2
25504-creator <25504@occ.ac.jp>
2026-04-23 16:56:22 +0900

    text style change

---
+++ main.js
@@ -0,0 +1,14 @@
+//const message = "Hello, World!";
+//alert(message);
+
+// const name = prompt("名前は？");
+// alert("こんにちは " + name + " さん！");
+
+//const answer = confirm("削除しますか？");
+//console.log(answer);
+
+const target = document.getElementById("myButton");
+target.textContent = "クリックしてね！";
+target.style.color = "red";
+target.style.fontSize = "20px";
+target.style.backgroundColor = "yellow";
